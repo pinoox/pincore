7253220480ac207f738a9ccaf7c31b815d29a7b8
yoosefap <yoosefalipour@gmail.com>
2026-06-20 02:44:06 +0330

    fix: read manager version from config in PathUrlSystemTest

--- tests/NonIsolated/Feature/Routing/PathUrlSystemTest.php
+++ tests/NonIsolated/Feature/Routing/PathUrlSystemTest.php
@@ -210,6 +210,7 @@ it('reads app manifest through app accessor', function () {
     $manifest = $url->appAccessor('com_pinoox_manager');
     $expectedLang = \Pinoox\Portal\App\AppEngine::config('com_pinoox_manager')->get('lang');
     $expectedName = \Pinoox\Component\Package\AppManifest::displayName('com_pinoox_manager');
+    $expectedVersion = \Pinoox\Portal\App\AppEngine::config('com_pinoox_manager')->get('version-name');
 
     expect($manifest->package())->toBe('com_pinoox_manager')
         ->and($manifest->name())->toBe($expectedName)
@@ -223,7 +224,7 @@ it('reads app manifest through app accessor', function () {
         ->and($manifest->resource('resources/avatar.png'))
         ->toBe('http://localhost/pinoox/apps/com_pinoox_manager/resources/avatar.png')
         ->and($manifest->theme()->name())->toBe('spark')
-        ->and($manifest->versionName())->toBe('2.2.0')
+        ->and($manifest->versionName())->toBe($expectedVersion)
         ->and(app('com_pinoox_manager')->name())->toBe($expectedName)
         ->and(app('com_pinoox_manager')->config('lang'))->toBe($expectedLang)
         ->and(app('com_pinoox_manager'))->toBeInstanceOf(\Pinoox\Component\Path\AppAccessor::class);
