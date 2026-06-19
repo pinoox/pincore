0ea0dcfbbae8243dcca3362e89d3b3b20455a2b5
yoosefap <yoosefalipour@gmail.com>
2026-06-19 20:31:35 +0330

    fix: return 422 API envelope for ValidationException on login and API routes

--- Component/Kernel/Listener/ExceptionListener.php
+++ Component/Kernel/Listener/ExceptionListener.php
@@ -13,15 +13,14 @@
 
 namespace Pinoox\Component\Kernel\Listener;
 
+use Illuminate\Validation\ValidationException as IlluminateValidationException;
+use Pinoox\Component\Http\Api\ApiResponse;
 use Pinoox\Component\Http\Response;
 use Pinoox\Component\Http\ResponseException;
-use Pinoox\Component\Kernel\Exception;
-use Pinoox\Portal\Kernel\Dispatcher;
-use Pinoox\Portal\Kernel\Listener;
-use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionListener implements EventSubscriberInterface
@@ -29,11 +28,27 @@ class ExceptionListener implements EventSubscriberInterface
     public function onException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
+
         if ($exception instanceof ResponseException) {
             $event->setResponse(
                 $exception->getResponse()
             );
-        } else if ($event->getRequest()->attributes->has('_controller')) {
+
+            return;
+        }
+
+        if ($exception instanceof IlluminateValidationException) {
+            $message = $exception->validator->errors()->first() ?: $exception->getMessage();
+
+            $event->setResponse(
+                ApiResponse::error('VALIDATION_FAILED', $message, $exception->errors(), 422, translate: false),
+            );
+            $event->allowCustomResponseCode();
+
+            return;
+        }
+
+        if ($event->getRequest()->attributes->has('_controller')) {
             $controller = $event->getRequest()->attributes->get('_controller');
             if (is_array($controller) && isset($controller[0]) && class_exists($controller[0]) && method_exists($controller[0], '_exception')) {
                 $result = call_user_func_array(array($controller[0], '_exception'), [
