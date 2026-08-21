bb76e4e900360fd6c33728f4d875165830cae85e
SAeed.ajf <ajfandaks@gmail.com>
2026-08-21 19:47:13 +0330

    feat(pinx): include runtime requirements in builds

--- Component/Package/Pinx/PinxBuilder.php
+++ Component/Package/Pinx/PinxBuilder.php
@@ -54,9 +54,10 @@ class PinxBuilder
         }
 
         $manifest = PinxManifest::fromAppConfig($appConfig, $build['type'], [
-            'target_app' => $build['target_app'],
-            'theme_name' => $build['theme_name'],
-            'minpin' => $build['minpin'],
+        'target_app' => $build['target_app'],
+        'theme_name' => $build['theme_name'],
+        'minpin' => $build['minpin'],
+        'requirements' => $build['requirements'],
         ]);
 
         $packagePath = $this->engine->path($package);
@@ -113,6 +114,7 @@ class PinxBuilder
      *     target_app: string,
      *     theme_name: string,
      *     minpin: int,
+     *     requirements: array<string, string>,
      *     gitignore: bool,
      *     exclude: list<string>,
      *     include_themes: list<string>,
